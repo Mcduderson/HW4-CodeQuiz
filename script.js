af7a542912a65ace82c5d5ec56fc379e49241f69
Elliott Payne <e.payne000@gmail.com>
2020-09-28 15:30:56 -0500

    Filled out html a bit.

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 // Create Oject to hold the question and the choices for each.
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
 
+];
+
+var score = 0;
+var 
 
 //Create event lister click that starts the test off.
+////kick off timer
 
+// Timer that decreases by ~15 sec for each wrong answer
+// use bubblimg up to control correct vs incorrect.
+// On click, change text from object
 
 //
